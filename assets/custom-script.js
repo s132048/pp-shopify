36c3f8f9e5b10c50aa73b5d4088785f9e4826bf9
CHOYJ145 <74890282+CHOYJ145@users.noreply.github.com>
2023-05-19 19:23:37 +0900

    cart

--- assets/custom-script.js
+++ assets/custom-script.js
@@ -1,8 +1,6 @@
 const menuBtn = document.querySelector('.menu-item--nav');
 const navPanel = document.querySelector('.nav-wrapper');
 const navCloseBtn = document.querySelector('.nav-close-btn');
-const filterBtn = document.querySelector('.filter-btn');
-const filter = document.querySelector('.facets-wrap');
 
 menuBtn.addEventListener('click', function() {
   navPanel.classList.add('active');
@@ -10,8 +8,4 @@ menuBtn.addEventListener('click', function() {
 
 navCloseBtn.addEventListener('click', function() {
   navPanel.classList.remove('active');
-});
-
-filterBtn.addEventListener('click', function() {
-  filter.classList.toggle('on');
 });
