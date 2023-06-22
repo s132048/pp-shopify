5ea0bf68760af1422f964397b84ec28e4ea72c25
CHOYJ145 <74890282+CHOYJ145@users.noreply.github.com>
2023-06-22 22:25:55 +0900

    10:25

--- assets/custom-script.js
+++ assets/custom-script.js
@@ -1,6 +1,9 @@
 const menuBtn = document.querySelector('.menu-item--nav');
 const navPanel = document.querySelector('.nav-wrapper');
 const navCloseBtn = document.querySelector('.nav-close-btn');
+const SearchCloseBtn = document.querySelector('.search-modal__close-button');
+const SearchPanel = document.querySelector('.pp .search-modal');
+
 
 menuBtn.addEventListener('click', function() {
   navPanel.classList.add('active');
@@ -8,4 +11,9 @@ menuBtn.addEventListener('click', function() {
 
 navCloseBtn.addEventListener('click', function() {
   navPanel.classList.remove('active');
+});
+
+SearchCloseBtn.addEventListener('click', function() {
+  SearchPanel.style.width = '100%';
+  SearchPanel.style.height = '100%';
 });
